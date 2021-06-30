ed5ce842de5c0c1025862ff1dddf3f611539ce20
Alistair <deadlyhardware@gmail.com>
2021-06-29 22:03:47 -0400

    Now able to add data to the table via the form

--- src/app/components/budget.component.ts
+++ src/app/components/budget.component.ts
@@ -22,13 +22,36 @@ export class BudgetComponent implements OnInit {
     constructor() { };
 
     budgetInput: number = 100;
-    budgetTotal: number = 100.00;
-
+    budgetTotal: number = 100;
     expenses: Expenses[] = [];
 
+    input: Expenses = {
+        id: 0,
+        expenseName: "",
+        costAmount: 0,
+        priority: "Medium",
+        desiredPercentage: 0,
+        actualPercentage: 0
+    }
+
     updateBudgetAmount(budget: number): void {
-        console.log(budget)
         this.budgetTotal = budget;
+    }
+
+    addExpense(): void {
+        let newExpenseObj: Expenses = {
+            id: 0,
+            expenseName: this.input.expenseName,
+            costAmount: this.input.costAmount,
+            priority: this.input.priority,
+            desiredPercentage: this.input.desiredPercentage,
+            actualPercentage: 0
+        }
+        
+        this.expenses.push(newExpenseObj);
+    }
+    cancelExpense(): void {
+
     }
 
 }
