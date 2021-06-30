2db15fcb1396ce608b975ff4aa44a90720fd8f58
Alistair <deadlyhardware@gmail.com>
2021-06-29 23:13:00 -0400

    Begin work on evalutateActualPercentage function

--- src/app/components/budget.component.ts
+++ src/app/components/budget.component.ts
@@ -15,15 +15,13 @@ interface Expenses {
   templateUrl: "./budget.component.html"
 })
 
-
-
 export class BudgetComponent implements OnInit {
+
     ngOnInit(): void {};
     constructor() { };
 
     budgetInput: number = 100;
     budgetTotal: number = 100;
-    expenses: Expenses[] = [];
 
     input: Expenses = {
         id: "",
@@ -33,37 +31,45 @@ export class BudgetComponent implements OnInit {
         desiredPercentage: 0,
         actualPercentage: 0
     }
+    expenses: Expenses[] = [];
+
 
     updateBudgetAmount(budget: number): void {
         this.budgetTotal = budget;
+        this.evaluateActualPercentage();
     }
 
     addExpense(): void {
+    // Make sure the user actually fills out all of the fields on the form
+    if(!this.input.expenseName || !this.input.costAmount || !this.input.desiredPercentage) return alert("Please fill out all fields!");
 
-        // Make sure the user actually fills out all of the fields on the form
-        if(!this.input.expenseName || !this.input.costAmount || !this.input.desiredPercentage) return alert("Please fill out all fields!");
-
-        let newExpenseObj: Expenses = {
-            id: this.newUuid(),
-            expenseName: this.input.expenseName,
-            costAmount: this.input.costAmount,
-            priority: this.input.priority,
-            desiredPercentage: this.input.desiredPercentage,
-            actualPercentage: 0
-        }
-
-        this.expenses.push(newExpenseObj);
-        console.table(newExpenseObj);
+    let newExpenseObj: Expenses = {
+        id: this.newUuid(),
+        expenseName: this.input.expenseName,
+        costAmount: this.input.costAmount,
+        priority: this.input.priority,
+        desiredPercentage: this.input.desiredPercentage,
+        actualPercentage: 0
     }
+
+    this.expenses.push(newExpenseObj);
+    }
+
     cancelExpense(): void {
 
     }
-
+    
+    // This function returns a unique ID
     newUuid() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           let r = Math.random() * 16 | 0, res = c == 'x' ? r : (r & 0x3 | 0x8);
           return res.toString(16);
         });
+    }
+
+    evaluateActualPercentage(): void {
+        // work on this tomorrow
+        console.log("working");
     }
       
 }
