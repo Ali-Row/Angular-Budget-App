c7f75621e9448f11902837a700287e5e20470279
Alistair <deadlyhardware@gmail.com>
2021-06-29 20:34:26 -0400

    Work on typescript logic for budget component

--- src/app/components/budget.component.ts
+++ src/app/components/budget.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 
+interface Expenses {
+    id: number,
+    expenseName: string,
+    costAmount: number,
+    priority: string,
+    desiredPercentage: number,
+    actualPercentage: number
+}
+
 @Component({
   selector: "app-budget",
   styleUrls: ["./budget.component.scss"],
   templateUrl: "./budget.component.html"
 })
 
-export class BudgetComponent implements OnInit {
-    ngOnInit(): void {
 
+
+export class BudgetComponent implements OnInit {
+    ngOnInit(): void {};
+    constructor() { };
+
+    budgetInput: number = 100;
+    budgetTotal: number = 100.00;
+
+    expenses: Expenses[] = [];
+
+    updateBudgetAmount(budget: number): void {
+        console.log(budget)
+        this.budgetTotal = budget;
     }
 
 }
